06cd16a13dc88f05fc3c8c2908ef0df69e58a58d
Chris Gonzales <cm.gonzales@yahoo.com>
2017-11-03 18:57:25 -0700

    added bootstrap

--- app.js
+++ app.js
@@ -16,19 +16,25 @@ app.use(function(req, res, next){
 }); 
 // Index route
 app.get('/', (req, res) => {
-    const title = 'Welcome1';
-   res.render('index', {title: title
+    const title = 'Welcome to the  ';
+   res.render('INDEX', {title: title
 });
 });
-// About rez
+// About rout
 app.get('/about', (req,res) =>{
 res.render('about');
 });
+//Search route
+
+app.get("/search", (req, res) =>{
+    res.render('search');
+});
+
 
 const port = 5000;
 
 app.listen(port, () =>{
-    console.log("Server started on port  " + port);
+    console.log("Server started on port " + port);
 });
 
 
