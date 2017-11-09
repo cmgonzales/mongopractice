2d8060a8402984d75ca5092595bb05d9f9174ca3
Chris Gonzales <cm.gonzales@yahoo.com>
2017-11-08 19:04:28 -0800

    added body parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -13,10 +14,10 @@ mongoose.connect('mongodb://localhost/vidjot-dev',{
 useMongoClient: true
 })
 .then(()=> console.log('Mongodb Connected....'))
-.catch(err => console.log("error"));
+.catch(err => console.log(err));
 
 // Load Idea Model
-require('./models.Idea');
+require('./models/Idea');
 const Idea = mongoose.model('ideas');
 
 //handlebars middleware
@@ -24,30 +25,57 @@ const Idea = mongoose.model('ideas');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// Body parser access what ever submited in textfirld
+app.use(bodyParser.urlencoded({ extended:false}))
+app.use(bodyParser.json())
 
-//how middleware works
-app.use(function(req, res, next){
-    //console.log(Date.now());
-    req.name = 'chris';
-    next();
-}); 
+
+// //how middleware works
+// app.use(function(req, res, next){
+//     //console.log(Date.now());
+//     req.name = 'chris';
+//     next();
+// }); 
 // Index route
 app.get('/', (req, res) => {
-    const title = 'Welcom';
+    const title = 'Welcome';
    res.render('INDEX', {title: title
 });
 });
-// About rout
-app.get('/about', (req,res) =>{
+// About route
+app.get('/about', (req, res) =>{
 res.render('about');
 });
 
+//Add Idea Form
+app.get('/ideas/add', (req, res) =>{
+    res.render('ideas/add');
+    });
 
+    //process form
+    app.post('/ideas', (req, res) => {
+       let errors = []
+
+       if(!req.body.title){
+           errors.push({text:'Please add a title'});
+       }
+       if(!req.body.details){
+           errors.push({text: 'Please add some details'})
+       }
+       if(errors.length > 0){
+           res.render('ideas/add', {
+               errors: errors,
+               title: req.body.title,
+               detals:req.body.details
+           });
+       }else{
+           res.send('passed');
+       }
+    });
 
 const port = 5000;
 
 app.listen(port, () =>{
     console.log("Server started on port " + port);
 });
-
 
