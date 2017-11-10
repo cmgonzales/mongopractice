faa88c0d691d74aeb14db8db9636e71046b8f3f5
Chris Gonzales <cm.gonzales@yahoo.com>
2017-11-09 20:14:43 -0800

    added new items

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -14,7 +15,7 @@ mongoose.connect('mongodb://localhost/vidjot-dev',{
 useMongoClient: true
 })
 .then(()=> console.log('Mongodb Connected....'))
-.catch(err => console.log(err));
+.catch(err => console.log("connection error"));
 
 // Load Idea Model
 require('./models/Idea');
@@ -26,9 +27,11 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Body parser access what ever submited in textfirld
-app.use(bodyParser.urlencoded({ extended:false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
 
+// Method override middleware
+app.use(methodOverride('_method'));
 
 // //how middleware works
 // app.use(function(req, res, next){
@@ -52,6 +55,32 @@ app.get('/ideas/add', (req, res) =>{
     res.render('ideas/add');
     });
 
+    //edit Idea Form
+app.get('/ideas/edit/:id', (req, res) =>{
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea =>{
+        res.render('ideas/edit',{
+            idea:idea
+        });
+    });
+    res.render('ideas/edit');
+    });
+
+
+    //Idea Index page
+    app.get('/ideas', (req, res) =>{
+        Idea.find({})
+        .sort({date: "desc"})
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas:ideas
+            });
+        });
+        res.render('ideas/index');
+    });
+
     //process form
     app.post('/ideas', (req, res) => {
        let errors = []
@@ -60,7 +89,7 @@ app.get('/ideas/add', (req, res) =>{
            errors.push({text:'Please add a title'});
        }
        if(!req.body.details){
-           errors.push({text: 'Please add some details'})
+           errors. push({text: 'Please add some details'})
        }
        if(errors.length > 0){
            res.render('ideas/add', {
@@ -69,9 +98,43 @@ app.get('/ideas/add', (req, res) =>{
                detals:req.body.details
            });
        }else{
-           res.send('passed');
+           const newUser = {
+               title: req.body.title,
+               details: req.body.details,
+               
+           }
+           new Idea(newUser)
+           .save()
+           .then(idea => {
+               res.redirect('/ideas');
+           })
        }
     });
+
+//Edit form process
+app.put('/ideas/:id', (req, res) =>{
+    Idea.findOne({
+        _id: req.params._id
+    })
+    .then(idea =>{
+        //new values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+
+        idea.save()
+        .then(idea => {
+            res.redirect('/ideas');
+        })
+    })
+});
+
+//delete idea
+app.delete('/ideas/:id',(req, res) =>{
+    Idea.remove({_id: req.params.id})
+    .then(() => {
+        res.redirect('/ideas');
+    })
+});
 
 const port = 5000;
 
